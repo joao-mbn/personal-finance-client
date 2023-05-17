36cd0a778eb6349754f1aaa7adbe011fb0b1ce55
João Nascimento <joaombn97@gmail.com>
2023-05-17 07:11:27 -0300

    Trying hardcoded url for requests using the wrong url in deployed version.

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const { VITE_API_BASE_URL } = import.meta.env;
 
 export const api = axios.create({
-  baseURL: VITE_API_BASE_URL,
+  baseURL: 'https://personal-finance-api.azurewebsites.net/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
