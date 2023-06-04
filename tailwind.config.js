8276c33b95953edda89a31dbf0ecd868d4df8b7c
João Nascimento <joaombn97@gmail.com>
2023-06-04 15:30:50 -0300

    Added prettier extension to organize tailwind classes. Added primary color. Added preflight config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,26 @@ export default {
       fontSize: {
         tiny: ['0.6rem', { lineHeight: '0.75rem' }],
       },
+      colors: {
+        cerulean: {
+          50: '#f0f9ff',
+          100: '#e1f1fd',
+          200: '#bbe5fc',
+          300: '#80cff9',
+          400: '#3cb8f4',
+          500: '#13a4ec',
+          600: '#067fc3',
+          700: '#06659e',
+          800: '#0a5582',
+          900: '#0e476c',
+          950: '#092e48',
+        },
+      },
+      fontFamily: {
+        sans: ['"Open Sans"', 'sans-serif'],
+      },
     },
   },
   important: '#root',
   plugins: [],
-  corePlugins: {
-    preflight: false,
-  },
 };
