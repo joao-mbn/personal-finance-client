003cab202ea603f1ef85dd5c6922e68a2b24bafa
João Nascimento <joaombn97@gmail.com>
2023-06-04 21:00:16 -0300

    reverted base api url change by mistake.

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'https://personal-finance-api.azurewebsites.net/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
