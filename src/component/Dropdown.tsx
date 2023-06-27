6196d5c600072e070f02385757980a8bf8463a97
João Nascimento <joaombn97@gmail.com>
2023-06-27 06:50:32 -0300

    Finished Dropdown MVP.

--- src/component/Dropdown.tsx
+++ src/component/Dropdown.tsx
@@ -1,11 +1,12 @@
 import classNames from 'classnames';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import { Button } from './Button';
 import { ChevronIcon } from './Icons';
 
 interface DropdownProps<T> {
   options: T[];
   onChange: (option: T, event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  multiSelect?: boolean;
   placeholder?: string;
   selected?: T[];
   template?: (option: T) => ReactNode;
@@ -14,34 +15,51 @@ interface DropdownProps<T> {
 export function Dropdown<T extends { key: number | string; value: ReactNode; disabled?: boolean }>({
   onChange,
   options,
+  multiSelect = false,
   placeholder = ' ',
   selected = [],
   template,
 }: DropdownProps<T>) {
   const [isActive, setIsActive] = useState(false);
-  console.log(selected);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const getPath = (event: MouseEvent) => {
+      const div = containerRef.current;
+      if (!div) return setIsActive(false);
+
+      const path = event.composedPath();
+      const shouldClose = !path.includes(containerRef.current);
+      shouldClose && setIsActive(false);
+    };
+    window.addEventListener('click', getPath);
+
+    return () => window.removeEventListener('click', getPath);
+  }, []);
 
   return (
-    <div className="w-32 p-1">
+    <div
+      className="w-32 p-1"
+      ref={containerRef}>
       <Button
         className="w-full"
         iconPosition="right"
         importance="secondary"
+        onClick={() => setIsActive(prev => !prev)}
         size="small"
         icon={
           <ChevronIcon
             viewBox="-12 -16 48 48"
             className={classNames('w-6 fill-none stroke-slate-500 stroke-2', {
-              'rotate-180': isActive,
+              'rotate-180': !isActive,
             })}
           />
         }
         label={
-          <span className="w-24 truncate pl-2 text-left">
+          <span className="mr-auto w-2/3 truncate pl-2 text-left">
             {selected.length ? selected.map(s => s.value).join(', ') : placeholder}
           </span>
         }
-        onClick={() => setIsActive(prev => !prev)}
       />
       <div className="max-h-[10rem] overflow-y-auto  rounded-xl pt-1 text-xs text-slate-600 shadow-lg shadow-slate-400">
         <div className={classNames('z-10 flex flex-col', { hidden: !isActive })}>
@@ -56,6 +74,7 @@ export function Dropdown<T extends { key: number | string; value: ReactNode; dis
                 })}
                 onClick={event => {
                   !disabled && onChange(option, event);
+                  !multiSelect && setIsActive(false);
                 }}>
                 {template?.(option) ?? value}
               </div>
