e3978d302915f0f1c8fc0b6cc7eb0413b5b51307
João Nascimento <joaombn97@gmail.com>
2023-09-02 08:19:04 -0300

    Removed unused message/field enums.

--- src/models/enum.ts
+++ src/models/enum.ts
@@ -7,30 +7,3 @@ export enum DashboardWidget {
   Assets = 6,
   MonthlyDebts = 7,
 }
-
-export enum Message {
-  anErrorOccurred = 'anErrorOccurred',
-  belowMinLength = 'belowMinLength',
-  cannotBeNullOrZero = 'cannotBeNullOrZero',
-  editingNotAllowedEntry = 'editingNotAllowedEntry',
-  entryWasNotAssociated = 'entryWasNotAssociated',
-  entryWasNotCreated = 'entryWasNotCreated',
-  entryWasNotDeleted = 'entryWasNotDeleted',
-  entryWasNotUpdated = 'entryWasNotUpdated',
-  exceededMaxLength = 'exceededMaxLength',
-  invalidSessionId = 'invalidSessionId',
-  isRequired = 'isRequired',
-  noIdToken = 'noIdToken',
-  noSessionId = 'noSessionId',
-  sessionExpired = 'sessionExpired',
-  tokenWithoutInfo = 'tokenWithoutInfo',
-  userNotFound = 'userNotFound',
-}
-
-export enum Field {
-  target = 'target',
-  comment = 'comment',
-  type = 'type',
-  value = 'value',
-  timestamp = 'timestamp',
-}
